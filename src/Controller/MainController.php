8cb1e677ceffc9c5dafc87f7e030a8a2b61895c7
Aimad Imara <aimadimara99@gmail.com>
2024-05-02 11:56:35 +0100

    code fix by php-cs-fixer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController
 {
     #[Route('/')]
-    public function homepage(): Response {
+    public function homepage(): Response
+    {
         return new Response('<h1>StarShop</h1>');
     }
 }
